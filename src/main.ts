0aac18741a2469407810fc23af8605a1458fa329
shian15810 <shian15810@gmail.com>
2021-07-03 06:14:16 +0800

    integrate dotenv as config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestApplication, NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import type { Config } from './config';
+import { Default } from './config';
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  const nodeEnv = configService.get<Config['NODE_ENV']>(
+    'NODE_ENV',
+    Default.NODE_ENV,
+  );
+
+  const port = configService.get<Config['PORT']>('PORT', Default.PORT);
+
+  await app.listen(port);
+
+  const url = await app.getUrl();
+
+  Logger.log(
+    `Nest application running in ${nodeEnv} environment`,
+    NestApplication.name,
+  );
+
+  Logger.log(`Nest application listening at ${url}`, NestApplication.name);
 };
 
 bootstrap();
