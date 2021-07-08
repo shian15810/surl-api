bc3ffbe221c91b3c541c535848ae36acd114d6ed
shian15810 <shian15810@gmail.com>
2021-07-08 10:40:22 +0800

    integrate helmet

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import type { ConfigGetOptions } from '@nestjs/config';
 import { ConfigService as NestConfigService } from '@nestjs/config';
 
 import { ConfigDefault } from './config.constants';
@@ -6,13 +7,10 @@ import type { Config } from './config.interface';
 
 @Injectable()
 export class ConfigService extends NestConfigService<Config> implements Config {
-  public override get<Key extends keyof Config>(
-    key: Key,
-  ): Config[Key] | undefined {
-    return super.get(key);
-  }
+  private readonly options: ConfigGetOptions = { infer: true };
 
-  public readonly NODE_ENV = this.get('NODE_ENV') ?? ConfigDefault.NODE_ENV;
+  public readonly NODE_ENV =
+    this.get('NODE_ENV', this.options) ?? ConfigDefault.NODE_ENV;
 
-  public readonly PORT = this.get('PORT') ?? ConfigDefault.PORT;
+  public readonly PORT = this.get('PORT', this.options) ?? ConfigDefault.PORT;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestApplication, NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AppModule } from './app.module';
-import { ConfigDefault } from './config/config.constants';
 import { ConfigService } from './config/config.service';
 import { LoggerService } from './logger/logger.service';
 
@@ -11,21 +11,20 @@ const bootstrap = async () => {
   const configService = app.get(ConfigService);
   const loggerService = app.get(LoggerService);
 
-  const NODE_ENV = configService.get('NODE_ENV') ?? ConfigDefault.NODE_ENV;
-  const PORT = configService.get('PORT') ?? ConfigDefault.PORT;
-
-  if (NODE_ENV !== 'test') {
+  if (configService.NODE_ENV !== 'test') {
     app.enableShutdownHooks();
   }
 
   app.useLogger(loggerService);
 
-  await app.listen(PORT);
+  app.use(helmet());
+
+  await app.listen(configService.PORT);
 
   const url = await app.getUrl();
 
   loggerService.log(
-    `Nest application running in ${NODE_ENV} environment`,
+    `Nest application running in ${configService.NODE_ENV} environment`,
     NestApplication.name,
   );
   loggerService.log(
