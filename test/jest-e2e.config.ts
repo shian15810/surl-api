424c7d622fb96bc028f7cecd4679ee7b926bb86f
shian15810 <shian15810@gmail.com>
2021-07-17 22:31:55 +0800

    convert jest config to typescript

---
+++ test/jest-e2e.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.e2e-spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
+
+export default config;
